91e9e43f045726c726a3038d932cb0ded5e1be6f
Sofia Rodriguez del Busto <rdbsofi@gmail.com>
2024-11-08 10:49:25 -0300

    Controller y Views de Clientes listo

---
+++ Controllers/ClientesController.cs
@@ -0,0 +1,69 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace MiWebApi.Controllers;
+
+
+public class ClientesController : Controller
+{
+
+    private readonly ILogger<ClientesController> _logger;
+
+    private ClientesRepository repoClientes;
+
+    public ClientesController(ILogger<ClientesController> logger)
+    {
+        _logger = logger;
+        repoClientes = new ClientesRepository();
+    }
+
+    public IActionResult Index()
+    {
+        return View(repoClientes.ObtenerClientes());
+    }
+
+        [HttpGet]
+    public IActionResult AltaCliente()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult CrearCliente(Cliente cliente)
+    {
+        repoClientes.CrearCliente(cliente);
+        return RedirectToAction ("Index");
+
+    }
+
+    [HttpGet]
+    public IActionResult ModificarCliente(int id)
+    {
+        var Cliente  = repoClientes.ObtenerCliente(id);
+        return View(Cliente);
+    }
+
+    [HttpPost]
+    public IActionResult ModificarCliente(Cliente cliente)
+    {
+        repoClientes.ModificarCliente(cliente);
+        return RedirectToAction ("Index"); 
+
+    }
+
+    [HttpGet]
+    public IActionResult EliminarCliente(int id)
+    {
+        return View(repoClientes.ObtenerCliente(id));
+    }
+
+    [HttpGet]
+    public IActionResult EliminarClientePorId(int id)
+    {
+        repoClientes.EliminarCliente(id);
+        return RedirectToAction ("Index"); 
+    }
+
+    
+
+}
---
+++ Repositories/ClientesRepository.cs
@@ -0,0 +1,117 @@
+
+using Microsoft.Data.Sqlite;
+
+public class ClientesRepository
+{
+    string connectionString;
+
+    public ClientesRepository()
+    {
+        connectionString = @"Data Source = db/Tienda.db;Cache=Shared";
+    }
+
+
+    public void CrearCliente(Cliente cliente)
+    {
+        string query = @"INSERT INTO Cliente (Nombre, Email, Telefono) VALUES (@nombre, @email, @telefono)";
+
+        using (SqliteConnection connection = new SqliteConnection(connectionString))
+        {
+            connection.Open();
+            SqliteCommand command = new SqliteCommand(query, connection);
+            command.Parameters.AddWithValue("@nombre", cliente.Nombre);
+            command.Parameters.AddWithValue("@email", cliente.Email);
+            command.Parameters.AddWithValue("@telefono", cliente.Telefono);
+            command.ExecuteNonQuery();
+            connection.Close();
+        }
+    }
+
+    public List<Cliente> ObtenerClientes()
+    {
+        List<Cliente> clientes = new List<Cliente>();
+
+        string query = @"SELECT * FROM Cliente";
+
+        using (SqliteConnection connection = new SqliteConnection(connectionString))
+        {
+            connection.Open();
+            SqliteCommand command = new SqliteCommand(query, connection);
+
+            using (SqliteDataReader reader = command.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    Cliente nuevoCliente = new Cliente();
+                    nuevoCliente.ClienteId = Convert.ToInt32(reader["idCliente"]);
+                    nuevoCliente.Nombre = reader["Nombre"].ToString();
+                    nuevoCliente.Email = reader["Email"].ToString();
+                    nuevoCliente.Telefono = reader["Telefono"].ToString();
+                    clientes.Add(nuevoCliente);
+                }
+            }
+            connection.Close();
+        }
+        return clientes;
+    }
+
+    public void ModificarCliente(Cliente cliente)
+    {
+        string query = @"UPDATE Cliente SET Nombre = @nombre, Email = @email, Telefono = @telefono WHERE idCliente = @Id";
+
+        using (SqliteConnection connection = new SqliteConnection(connectionString))
+        {
+            connection.Open();
+            SqliteCommand command = new SqliteCommand(query, connection);
+            command.Parameters.AddWithValue("@nombre", cliente.Nombre);
+            command.Parameters.AddWithValue("@email", cliente.Email);
+            command.Parameters.AddWithValue("@telefono", cliente.Telefono);
+            command.Parameters.AddWithValue("@Id", cliente.ClienteId);
+            command.ExecuteNonQuery();
+            connection.Close();
+        }
+
+    }
+
+    public Cliente ObtenerCliente(int id)
+    {
+        Cliente cliente = null; //Uso el null para devolver en caso de no encontrar nada
+
+        string query = @"SELECT * FROM Cliente WHERE idCliente = @id ";
+
+        using (SqliteConnection connection = new SqliteConnection(connectionString))
+        {
+            connection.Open();
+            SqliteCommand command = new SqliteCommand(query, connection);
+            command.Parameters.AddWithValue("@id", id);
+            using (SqliteDataReader reader = command.ExecuteReader())
+            {
+                if (reader.Read())
+                {
+                    cliente = new Cliente();
+                    cliente.ClienteId = Convert.ToInt32(reader["idCliente"]);
+                    cliente.Nombre = reader["Nombre"].ToString();
+                    cliente.Email = reader["Email"].ToString();
+                    cliente.Telefono = reader["Telefono"].ToString();
+                }
+            }
+            connection.Close();
+        }
+        return cliente;
+    }
+
+    public void EliminarCliente(int id)
+    {
+        string query = @"DELETE FROM Cliente WHERE idCliente = @Id;";
+
+        using (SqliteConnection connection = new SqliteConnection(connectionString))
+        {
+            connection.Open();
+            SqliteCommand command = new SqliteCommand(query, connection);
+            command.Parameters.AddWithValue("@Id", id);
+            command.ExecuteNonQuery();
+            connection.Close();
+        }
+    }
+}
+}
